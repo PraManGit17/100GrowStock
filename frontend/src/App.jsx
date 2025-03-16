28555483c091e763fdd5879d499f7ef23d24fd04
Mir0069 <144161558+Mir0069@users.noreply.github.com>
2025-03-16 17:07:37 +0530

    fixedorb

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Landing from './pages/Landing/Landing';
 import Header from './components/Navbar/Header';
 import { Outlet } from 'react-router-dom';
-//  Orb from '../../../Frontend/Projects/MoneyMinder/frontend/src/Components/Orb/Orb';
+import Orb from "./components/Orb/Orb.jsx"
 
 function App() {
     return (
         <>
             <div className="px-4 backdrop-filter backdrop-blur-lg bg-opacity-30 border-4 shadow-inner border-gray-200 w-[96vw] md:mx-auto ml-2 rounded-xl">
+            
                 <Header />
                 <Orb />
                 <Outlet />
