1f7eab8030372190896c968dd5a94711d91e5112
BindingEnergy <santusht.matra11@hotmail.com>
2025-04-14 21:47:41 +0530

    Pushed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import Landing from './pages/Landing/Landing';
 import Navbar from './components/Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+
 // import Orb from "./components/Orb/Orb.jsx"
 // import Dashboard from './pages/Dashboard';
 function App() {
+    const navigate = useNavigate();
+    const [localToken, setLocalToken] = useState();
+    useEffect(() => {
+        const token = localStorage.getItem('access_token');
+        setLocalToken(token);
+        console.log(`Token: ${token}\nLocalToken:${localToken}`);
+    }, [navigate, localToken]);
     return (
         <>
             {/* <div className="px-4 backdrop-filter backdrop-blur-lg bg-opacity-30 border-4 shadow-inner border-gray-200 w-[95vw] md:mx-auto ml-2 rounded-xl overflow-x-hidden h-screen md:w-auto"> */}
             <div className="md:px-0 px-4 bg-black">
                 {/* <Orb /> */}
-                <Navbar />
+                {localToken != null ? <Navbar /> : ''}
+                {/* <Navbar /> */}
                 <Outlet />
-               {/* <Dashboard/>  */}
+                {/* <Dashboard/>  */}
             </div>
             {/* </div> */}
         </>
